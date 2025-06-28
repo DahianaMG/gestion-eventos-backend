a33dc9dfd8bccfef127bdfaf25962e0dbf2555ad
DahianaMG <122055480+DahianaMG@users.noreply.github.com>
2025-06-28 16:42:12 -0300

    Agrega archivo de rutas y autenticacion

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Validator;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role' => $request->role,
+        ]);
+
+        $token = $user->createToken('MyAppToken')->plainTextToken;
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Usuario registrado correctamente.',
+            'token' => $token,
+            'user' => $user,
+        ], 201);
+    }
+
+    // User Login API
+    public function login(Request $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        $user = Auth::user();
+        $token = $user->createToken('MyAppToken')->plainTextToken;
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Login successful.',
+            'token' => $token,
+            'user' => $user,
+        ]);
+    }
+
+    // User Profile API (Protected)
+    public function profile(Request $request)
+    {
+        return response()->json([
+            'success' => true,
+            'user' => $request->user(),
+        ]);
+    }
+
+    // User Logout API
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Logout successful.',
+        ]);
+    }
+}
